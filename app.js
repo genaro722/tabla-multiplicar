71b535b9f0f57b1b2a6fa33710f4d35766a5184b
Genaro <genaro.a@hotmail.com>
2020-05-15 18:33:46 -0700

    commit inicial

---
+++ app.js
@@ -0,0 +1,26 @@
+// const multiplicar = require('./multiplicar/multiplicar.js');
+const argv = require('./config/yargs').argv
+const colors = require('colors/safe')
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar.js');
+
+let comando = argv._[0]
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.blue(archivo)))
+            .catch(err => console.log(err))
+        break;
+
+    default:
+        console.log('Comando no reconocido');;
+}
+
+// let argv2 = process.argv;
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
